85e365f0a2147cccb0606d3124cb7b607ba25a4a
davidsudobit <davidmanson474@gmail.com>
2024-04-18 11:26:28 +0530

    Added Chart changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChartDemo';
+
+  barChartLabels=['OneOneOneOne', 'TwoTwoTwoTwo', 'ThreeThreeThreeThree', 'FourFourFourFour', 'FiveFiveFiveFive'];
+
+  barChartDatasets=[
+    {
+      data:[100,200,300,400,500]
+    }
+  ]
+
+  barChartOptions: ChartOptions<'pie'>={
+    plugins:{
+      legend: {
+        display: true,
+        position: 'right',
+        align: 'start'
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: true,
+    layout:{
+      padding: 4
+    },
+    aspectRatio: 2,
+  };
+
 }
